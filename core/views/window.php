4a5c5b09633ea29d161fb5a927e511a59b66d49e
Adam Fairholm <adam.fairholm@gmail.com>
2010-12-13 16:37:44 -0500

    Removing extra line on loading the window.

--- core/views/window.php
+++ core/views/window.php
@@ -1,9 +1,6 @@
 <form id="trigger_form">
 
-<textarea name="trigger_content" id="trigger_content">
-<?php foreach( $context as $item ): echo $item . " : "; endforeach; ?>
-
-</textarea>
+<textarea name="trigger_content" id="trigger_content"><?php foreach( $context as $item ): echo $item . " : "; endforeach; ?></textarea>
 
 <div id="trigger_target"></div>
 
