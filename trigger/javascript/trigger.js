2736e11bdf8b32e7db759bb78c34c1a63f3442ac
Adam Fairholm <adam.fairholm@gmail.com>
2010-12-20 13:42:31 -0500

    Removing hard coded URL from JS.

--- trigger/javascript/trigger.js
+++ trigger/javascript/trigger.js
@@ -10,7 +10,7 @@ $(document).ready(function()
 		dataType: "text",
 		type: "POST",
 		data: { XID:EE.XID, line:textarea },
-		url:  "http://localhost/ee/TCT/admin/"+EE.BASE+"&C=addons_modules&M=show_module_cp&module=trigger&method=parse_trigger_output",
+		url:  EE.BASE+"&C=addons_modules&M=show_module_cp&module=trigger&method=parse_trigger_output",
 		success: function(data){
 		
 		$('#trigger_content').val($('#trigger_content').val()+data);
