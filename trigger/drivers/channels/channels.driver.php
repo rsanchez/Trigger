e649ca50bf5a41e22e246cd15d6d8b73fcb720b9
Adam Fairholm <adam.fairholm@gmail.com>
2011-03-04 16:11:41 -0500

    Switching channels driver to newer format.

---
+++ trigger/drivers/channels/channels.driver.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Trigger Channels Driver
+ *
+ * @package		Trigger
+ * @author		Addict Add-ons Dev Team
+ * @copyright	Copyright (c) 2010 - 2011, Addict Add-ons
+ * @license		
+ * @link		
+ */
+class Driver_channels
+{
+	public $driver_version		= "0.9";
+	
+	public $driver_slug			= "channels";
+}
+
+/* End of file channels.driver.php */
--- trigger/drivers/channels/langauge/english/lang.channels.php
+++ trigger/drivers/channels/langauge/english/lang.channels.php
@@ -1,11 +1,22 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$command_lang = array(
+$driver_lang = array(
 
-	'site_offline'						=> "site is now offline",
-	'site_already_offline'				=> "site is already offline"
+	'driver_name'				=> "Channels",
+	'driver_desc'				=> "Controls EE channels.",
+
+	// --------------------------------------------------------------------------	
+
+	'no_data'					=> "no variable data provided",
+	'no_name'					=> "no global variable name provided",
+	'var_already_exits'			=> "this global variable already exists",
+	'global_add_success'		=> "global variable added successfully",
+	'global_add_error'			=> "there was a problem adding your global variable",
+	'no_variables'				=> "no global variables found",
+	'globals_delete_success'	=> "global variable deleted",
+	'global_set_success'		=> "global variable set successfully",
+	'global_set_error'			=> "there was a problem setting this global variable",
 
 );
 
-/* End of file lang.channels.php */
-/* Location: ./system/expressionengine/third_party/trigger/drivers/channels/language/english/lang.channels.php */
+/* End of file lang.snippets.php */
