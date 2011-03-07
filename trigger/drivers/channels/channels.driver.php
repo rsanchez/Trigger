c9266ccdebb6e8380f4f81c2155270214b8bf4e5
Adam Fairholm <adam.fairholm@gmail.com>
2011-03-07 15:53:15 -0500

    Channels - list, delete, new.

--- trigger/drivers/channels/channels.driver.php
+++ trigger/drivers/channels/channels.driver.php
@@ -27,9 +27,9 @@ class Driver_channels
 		// We're using the API for this dealio
 		// -------------------------------------
 		
-		$this->EE->load->library( 'Api' );
+		$this->EE->load->library('Api');
 		
-		$this->EE->api->instantiate( 'channel_structure' );
+		$this->EE->api->instantiate('channel_structure');
 	}
 
 	// --------------------------------------------------------------------------
@@ -41,22 +41,39 @@ class Driver_channels
 	 * @param	array
 	 * @return	string
 	 */
-	public function create_channel( $data )
+	public function _comm_new( $data )
 	{
+		$this->EE->load->helper('url');
+
 		// Use the current site ID
 		$channel_data['site_id']		= $this->EE->config->item('site_id');
+	
+		if(!is_array($data)):
 		
-		// Get some channel data
-		$channel_data['channel_name'] 	= $data['channel_name'];
-		$channel_data['channel_title']	= $data['channel_title'];
+			// Looks like we just have a title. Use it as a string.
+			$channel_data['channel_title'] 	= $data;
+			$channel_data['channel_name']	= url_title($data, 'underscore', TRUE);
 		
-		if( $this->EE->api->create_channel( $channel_data ) ):
+		elseif(isset($data[0]) && isset($data[1])):
 		
-			return "Channel added successfully.";
+			$channel_data['channel_name'] 	= $data[0];
+			$channel_data['channel_title']	= $data[1];
+		
+		else:
+		
+			return "insufficient data";
+
+		endif;
+	
+		// Create the channel	
+
+		if($this->EE->api_channel_structure->create_channel($channel_data)):
+		
+			return "channel added successfully";
 			
 		else:
 		
-			return "Error in adding channel.";
+			return "error in adding channel";
 		
 		endif;
 	}
@@ -64,32 +81,66 @@ class Driver_channels
 	// --------------------------------------------------------------------------
 
 	/**
-	 * Show Channels
+	 * List Channels
 	 *
 	 * @access	public
 	 * @return	string
 	 */	
-	function channels()
+	function _comm_list()
 	{
-		$call = $this->EE->api_channel_structure->get_channels();
+		$call = $this->EE->api_channel_structure->get_channels($this->EE->config->item('site_id'));
 		
 		if( !$call ):
 		
-			return "there are no channels";
+			return "no channels found";
 		
 		endif;
 		
 		$channels = $call->result();
 		
-		$out = "\n";
+		$out = TRIGGER_BUFFER."\n";
 		
 		foreach( $channels as $channel ):
 		
-			$out .= "$channel->channel_name => $channel->channel_title\n";
+			$out .= "$channel->channel_title ($channel->channel_name)\n";
 		
 		endforeach;
 		
-		return $out;
+		return $out .= TRIGGER_BUFFER;
+	}
+
+	// --------------------------------------------------------------------------
+
+	/**
+	 * Delete Channel
+	 *
+	 * @access	public
+	 * @return	string
+	 */	
+	function _comm_delete($channel_name)
+	{
+		// Get the ID
+		$this->EE->db->where('site_id', $this->EE->config->item('site_id'));
+		$query = $this->EE->db->limit(1)->where('channel_name', $channel_name)->get('channels');
+	
+		if($query->num_rows() == 0):
+		
+			return "cannot find channel";
+		
+		endif;
+		
+		$row = $query->row();
+		$channel_id = $row->channel_id;
+	
+		if( $this->EE->api_channel_structure->delete_channel($channel_id, $this->EE->config->item('site_id')) === FALSE ):
+		
+			return "error encountered when deleting this channel";
+		
+		else:
+		
+			return "channel deleted successfully";
+		
+		endif;
 	}
 
 }
