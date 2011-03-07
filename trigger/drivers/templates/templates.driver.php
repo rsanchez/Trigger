a5660a3d98fdbba5098a96050b0c617a29ef5795
Adam Fairholm <adam.fairholm@gmail.com>
2011-03-07 16:18:54 -0500

    Allow as files, disallow as files, and set basepath. Set basepath doesn't seem to work.

--- trigger/drivers/templates/templates.driver.php
+++ trigger/drivers/templates/templates.driver.php
@@ -65,6 +65,100 @@ class Driver_templates
 		return trigger_lang('templates_synced');
 	}
 
+	// --------------------------------------------------------------------------
+
+	/**
+	 * Set templates to be allowed as files
+	 */	
+	function _comm_allow_as_files()
+	{
+		if ( ! $this->EE->cp->allowed_group('can_access_design') OR ! $this->EE->cp->allowed_group('can_admin_templates'))
+		{
+			return "no";
+		}
+		
+		if($this->_change_preference('save_tmpl_files_options', 'y')):
+		
+			return "templates can be saved as files now";
+		
+		else:
+		
+			return "templates can already be saved as files";
+		
+		endif;
+	}
+
+	// --------------------------------------------------------------------------
+
+	/**
+	 * Set templates to be allowed as files
+	 */	
+	function _comm_disallow_as_files()
+	{
+		if ( ! $this->EE->cp->allowed_group('can_access_design') OR ! $this->EE->cp->allowed_group('can_admin_templates'))
+		{
+			return "no";
+		}
+		
+		if($this->_change_preference('save_tmpl_files_options', 'n')):
+		
+			return "templates cannot be saved as files now";
+		
+		else:
+		
+			return "templates are already not able to be saved as files";
+		
+		endif;
+	}
+
+	// --------------------------------------------------------------------------
+
+	/**
+	 * Set the templates base
+	 */	
+	function _comm_set_base($base)
+	{
+		if(!$base):
+		
+			return "no basepath provided";
+		
+		endif;
+	
+		if ( ! $this->EE->cp->allowed_group('can_access_design') OR ! $this->EE->cp->allowed_group('can_admin_templates')):
+		
+			return "no";
+		
+		endif;
+
+		$config = array('tmpl_file_basepath' => $base);
+
+		$this->EE->config->update_site_prefs($config);
+		
+		return "template basepath set";
+	}
+
+	// --------------------------------------------------------------------------
+	
+	/**
+	 * Change a template preference
+	 */
+	function _change_preference( $item, $new_status )
+	{
+		$current_status = $this->EE->config->item($item);
+	
+		if( $current_status == $new_status ):
+		
+			return FALSE;
+		
+		endif;
+
+		$config = array($item => $new_status);
+
+		$this->EE->config->update_site_prefs($config);
+		
+		return TRUE;
+	}
+
 }
 
 /* End of file templates.driver.php */
