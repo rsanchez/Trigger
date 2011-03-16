3d02046880a5f40e174a049ca7893ce564991b6c
Adam Fairholm <adam.fairholm@gmail.com>
2011-03-16 14:55:02 -0400

    Packages driver.

---
+++ trigger/drivers/pack/language/english/lang.pack.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$driver_lang = array(
+
+	'driver_name'				=> "Pack",
+	'driver_desc'				=> "Controls Trigger Packages.",
+
+	// --------------------------------------------------------------------------	
+
+	'no_packages'				=> "no packages found"
+
+);
+
+/* End of file lang.pack.php */
---
+++ trigger/drivers/pack/pack.driver.php
@@ -0,0 +1,61 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Trigger Packages Driver
+ *
+ * @package		Trigger
+ * @author		Adam Fairholm (Addict Add-ons)
+ * @copyright	Copyright (c) 2010 - 2011, Addict Add-ons
+ * @license		
+ * @link		
+ */
+ 
+class Driver_pack
+{
+	public $driver_version		= '0.9';
+	
+	public $driver_slug			= 'pack';
+
+	// --------------------------------------------------------------------------
+
+	function __construct()
+	{
+		$this->EE =& get_instance();
+	}
+
+	// --------------------------------------------------------------------------
+
+	/**
+	 * List packages available
+	 *
+	 * @access	public
+	 * @return	string
+	 */	
+	public function _comm_list()
+	{
+		$this->EE->load->model('package_mdl');
+	
+		$packages = $this->EE->package_mdl->get_packages();
+
+		$total = count($packages);
+		
+		if($total == 0 or $packages === FALSE):
+		
+			return trigger_lang('no_packages');
+		
+		endif;
+		
+		$out = TRIGGER_BUFFER."\n";
+		
+		foreach($packages as $package_name => $data):
+		
+			$out .= $data['name']." ($package_name)\n";
+		
+		endforeach;
+		
+		return $out .= TRIGGER_BUFFER;
+	}	
+
+}
+
+/* End of file pack.driver.php */
